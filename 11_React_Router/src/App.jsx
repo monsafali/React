28f270af53c9d42dff18890853c61b57b0333b57
monsafali <monsafali272@gmail.com>
2024-11-08 20:58:57 +0500

    react routing

---
+++ 11_React_Router/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Header from "./Header/Header.jsx";
+import Footer from "./Footer/Footer.jsx";
+import Home from "./Home/HOme.jsx";
+
+const App = () => {
+  return <></>;
+};
+
+export default App;
